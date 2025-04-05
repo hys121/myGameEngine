e0d262771db45b5b75ea2381427c48e3fb843e5e
hys <1127386191@qq.com>
2025-04-05 14:06:24 +0800

    Setup basic Application and Entry Point.

---
+++ GE_test/src/Engine/Core.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef GE_PLATFORM_WINDOWS
+
+	#ifdef GE_BUILD_DLL
+		#define GENGINE_API __declspec(dllexport)
+	#else
+		#define GENGINE_API __declspec(dllimport)
+	#endif
+#else
+	#error GEngine only support Windows!
+#endif 
